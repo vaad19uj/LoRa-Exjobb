bd37ee172f1ec1ebe281533d5000e7abc9ae8d98
AntonAhlinder <55693307+AntonAhlinder@users.noreply.github.com>
2022-04-06 20:24:25 +0200

    Mean Standard Deviation
    Testbed1

---
+++ Testbed_1/mean_standard_deviation.py
@@ -0,0 +1,26 @@
+import re
+import statistics
+
+regex = '.*\s.*\s*.*(-\d+)' + (49 * '.*\s.*(-\d+)')
+for position in range(1,4):
+    #Address in github repository
+    f_new = open("C:\\Users\\myran\Desktop\\Enchipsdatorer\\LoRa-Exjobb\\Testbed_1\\Testbed1_results\\Position" + str(position) + "_MSD.txt", "w")
+    combined_MSD = [0]*11
+    for receiver in range(1,4):
+        #Address in github repository
+        f = open("C:\\Users\\myran\\Desktop\\Enchipsdatorer\\LoRa-Exjobb\\Testbed_1\\Testbed1_results\\Position" + str(position) + "\\Testbed1_data_rec" + str(receiver) + ".txt")
+        txt = f.read();
+        extracted_data = re.findall(regex, txt)
+        index = 0
+        for datarate in extracted_data:
+            array = []
+            array.extend(datarate)
+            array_int = [int(i) for i in array]
+            combined_MSD[index]  += statistics.stdev(array_int)
+            print(combined_MSD[index])
+            index +=1
+    print(combined_MSD)
+    index = 0
+    for msd in combined_MSD:
+        f_new.write("DR" + str(index) + "\t" + str(msd / 3) + "\n")
+        index +=1
