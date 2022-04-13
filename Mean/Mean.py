6c5ebbfdadf32d282a7e228d92022c4ea42f153f
vaad19uj <vaad19uj@student.ju.se>
2022-04-13 09:11:09 +0200

    Distances

--- Mean/Mean.py
+++ Mean/Mean.py
@@ -8,11 +8,54 @@ def convertTuple(tup):
     st = ''.join(map(str, tup))
     return st
 
+def rssitodistance(rssi, dr):
+    k = 0
+    m = 0
+
+    if dr == 0:
+        k = -0.3238
+        m = -21.35
+    elif dr == 1:
+        k = -0.3254
+        m = -21.34
+    elif dr == 2:
+        k = -0.3279
+        m = -21.414
+    elif dr == 3:
+        k = -0.3077
+        m = -19.833
+    elif dr == 4:
+        k = -0.3028
+        m = -19.287
+    elif dr == 5:
+        k = -0.2952
+        m = -18.428
+    elif dr == 6:
+        k = -0.2832
+        m = -17.128
+    elif dr == 7:
+        k = -0.2707
+        m = -16.23
+    elif dr == 8:
+        k = -0.2854
+        m = -17.045
+    elif dr == 9:
+        k = -0.2794
+        m = -16.067
+    elif dr == 10:
+        k = -0.2808
+        m = -15.69
+    return(k*rssi+m)
+
 regex = '.*\s.*\s*.*(-\d+)' + (49 * '.*\s.*(-\d+)')
 
-f_new_pos1 = open("C:\\Users\\Adina\\source\\repos\\Exjobb\\LoRa-Exjobb\\Testbed_1\\Testbed1_results\\Position1_Mean", "w")
-f_new_pos2 = open("C:\\Users\\Adina\\source\\repos\\Exjobb\\LoRa-Exjobb\\Testbed_1\\Testbed1_results\\Position2_Mean", "w")
-f_new_pos3 = open("C:\\Users\\Adina\\source\\repos\\Exjobb\\LoRa-Exjobb\\Testbed_1\\Testbed1_results\\Position3_Mean", "w")
+#f_new_pos1 = open("C:\\Users\\Adina\\source\\repos\\Exjobb\\LoRa-Exjobb\\Testbed_1\\Testbed1_results\\Position1_Mean", "w")
+#f_new_pos2 = open("C:\\Users\\Adina\\source\\repos\\Exjobb\\LoRa-Exjobb\\Testbed_1\\Testbed1_results\\Position2_Mean", "w")
+#f_new_pos3 = open("C:\\Users\\Adina\\source\\repos\\Exjobb\\LoRa-Exjobb\\Testbed_1\\Testbed1_results\\Position3_Mean", "w")
+
+f_new_pos1 = open("C:\\Users\\Adina\\source\\repos\\Exjobb\\LoRa-Exjobb\\Testbed_1\\Testbed1_results\\Distances\\Position1_Distance.txt", "w")
+f_new_pos2 = open("C:\\Users\\Adina\\source\\repos\\Exjobb\\LoRa-Exjobb\\Testbed_1\\Testbed1_results\\Distances\\Position2_Distance.txt", "w")
+f_new_pos3 = open("C:\\Users\\Adina\\source\\repos\\Exjobb\\LoRa-Exjobb\\Testbed_1\\Testbed1_results\\Distances\\Position3_Distance.txt", "w")
 
 frec1pos1 = open("C:\\Users\\Adina\\source\\repos\\Exjobb\\LoRa-Exjobb\\Testbed_1\\Testbed1_results\\Position1\\Testbed1_data_rec1.txt")
 frec2pos1 = open("C:\\Users\\Adina\\source\\repos\\Exjobb\\LoRa-Exjobb\\Testbed_1\\Testbed1_results\\Position1\\Testbed1_data_rec2.txt")
@@ -54,137 +97,274 @@ f_new_pos1.write("rec 1" + "\n")
 dr = 0
 for tup in extracted_data_rec1pos1:
     combined_mean = 0.0
-    print("DR " + str(dr))
     f_new_pos1.write("DR " + str(dr) + "\n")
-    dr += 1
     for tuptup in tup:
         print(tuptup)
         combined_mean += int(tuptup)
 
-    print("Mean: " + str(combined_mean/len(tup)))
-    mean = str(combined_mean/len(tup))
-    f_new_pos1.write("Mean: " + mean + "\n")
+    mean = combined_mean/len(tup)
+    distance = rssitodistance(mean, dr)
+    f_new_pos1.write("Mean: " + str(mean) + "\n")
+    f_new_pos1.write("Distance: " + str(distance) + "\n")
+    dr += 1
 
 f_new_pos1.write("rec 2" + "\n")
 dr = 0
 for tup in extracted_data_rec2pos1:
     combined_mean = 0.0
-    print("DR " + str(dr))
     f_new_pos1.write("DR " + str(dr) + "\n")
-    dr += 1
     for tuptup in tup:
         print(tuptup)
         combined_mean += int(tuptup)
 
-    print("Mean: " + str(combined_mean/len(tup)))
-    mean = str(combined_mean/len(tup))
-    f_new_pos1.write("Mean: " + mean + "\n")
+    mean = combined_mean/len(tup)
+    distance = rssitodistance(mean, dr)
+    f_new_pos1.write("Mean: " + str(mean) + "\n")
+    f_new_pos1.write("Distance: " + str(distance) + "\n")
+    dr += 1
 
 f_new_pos1.write("rec 3" + "\n")
 dr = 0
 for tup in extracted_data_rec3pos1:
     combined_mean = 0.0
-    print("DR " + str(dr))
     f_new_pos1.write("DR " + str(dr) + "\n")
-    dr += 1
     for tuptup in tup:
         print(tuptup)
         combined_mean += int(tuptup)
 
-    print("Mean: " + str(combined_mean/len(tup)))
-    mean = str(combined_mean/len(tup))
-    f_new_pos1.write("Mean: " + mean + "\n")
+    mean = combined_mean/len(tup)
+    distance = rssitodistance(mean, dr)
+    f_new_pos1.write("Mean: " + str(mean) + "\n")
+    f_new_pos1.write("Distance: " + str(distance) + "\n")
+    dr += 1
 
 f_new_pos2.write("rec 1" + "\n")
 dr = 0
 for tup in extracted_data_rec1pos2:
     combined_mean = 0.0
-    print("DR " + str(dr))
     f_new_pos2.write("DR " + str(dr) + "\n")
-    dr += 1
     for tuptup in tup:
         print(tuptup)
         combined_mean += int(tuptup)
 
-    print("Mean: " + str(combined_mean/len(tup)))
-    mean = str(combined_mean/len(tup))
-    f_new_pos2.write("Mean: " + mean + "\n")
+    mean = combined_mean/len(tup)
+    distance = rssitodistance(mean, dr)
+    f_new_pos2.write("Mean: " + str(mean) + "\n")
+    f_new_pos2.write("Distance: " + str(distance) + "\n")
+    dr += 1
 
 f_new_pos2.write("rec 2" + "\n")
 dr = 0
 for tup in extracted_data_rec2pos2:
     combined_mean = 0.0
-    print("DR " + str(dr))
     f_new_pos2.write("DR " + str(dr) + "\n")
-    dr += 1
     for tuptup in tup:
         print(tuptup)
         combined_mean += int(tuptup)
 
-    print("Mean: " + str(combined_mean/len(tup)))
-    mean = str(combined_mean/len(tup))
-    f_new_pos2.write("Mean: " + mean + "\n")
+    mean = combined_mean/len(tup)
+    distance = rssitodistance(mean, dr)
+    f_new_pos2.write("Mean: " + str(mean) + "\n")
+    f_new_pos2.write("Distance: " + str(distance) + "\n")
+    dr += 1
 
 f_new_pos2.write("rec 3" + "\n")
 dr = 0
 for tup in extracted_data_rec3pos2:
     combined_mean = 0.0
-    print("DR " + str(dr))
     f_new_pos2.write("DR " + str(dr) + "\n")
-    dr += 1
     for tuptup in tup:
         print(tuptup)
         combined_mean += int(tuptup)
 
-    print("Mean: " + str(combined_mean/len(tup)))
-    mean = str(combined_mean/len(tup))
-    f_new_pos2.write("Mean: " + mean + "\n")
-
+    mean = combined_mean/len(tup)
+    distance = rssitodistance(mean, dr)
+    f_new_pos2.write("Mean: " + str(mean) + "\n")
+    f_new_pos2.write("Distance: " + str(distance) + "\n")
+    dr += 1
 
 f_new_pos3.write("rec 1" + "\n")
 dr = 0
 for tup in extracted_data_rec1pos3:
     combined_mean = 0.0
-    print("DR " + str(dr))
     f_new_pos3.write("DR " + str(dr) + "\n")
-    dr += 1
     for tuptup in tup:
         print(tuptup)
         combined_mean += int(tuptup)
 
-    print("Mean: " + str(combined_mean/len(tup)))
-    mean = str(combined_mean/len(tup))
-    f_new_pos3.write("Mean: " + mean + "\n")
+    mean = combined_mean/len(tup)
+    distance = rssitodistance(mean, dr)
+    f_new_pos3.write("Mean: " + str(mean) + "\n")
+    f_new_pos3.write("Distance: " + str(distance) + "\n")
+    dr += 1
 
 f_new_pos3.write("rec 2" + "\n")
 dr = 0
 for tup in extracted_data_rec2pos3:
     combined_mean = 0.0
-    print("DR " + str(dr))
     f_new_pos3.write("DR " + str(dr) + "\n")
-    dr += 1
     for tuptup in tup:
         print(tuptup)
         combined_mean += int(tuptup)
 
-    print("Mean: " + str(combined_mean/len(tup)))
-    mean = str(combined_mean/len(tup))
-    f_new_pos3.write("Mean: " + mean + "\n")
+    mean = combined_mean/len(tup)
+    distance = rssitodistance(mean, dr)
+    f_new_pos3.write("Mean: " + str(mean) + "\n")
+    f_new_pos3.write("Distance: " + str(distance) + "\n")
+    dr += 1
 
 f_new_pos3.write("rec 3" + "\n")
 dr = 0
 for tup in extracted_data_rec3pos3:
     combined_mean = 0.0
-    print("DR " + str(dr))
     f_new_pos3.write("DR " + str(dr) + "\n")
-    dr += 1
     for tuptup in tup:
         print(tuptup)
         combined_mean += int(tuptup)
 
-    print("Mean: " + str(combined_mean/len(tup)))
-    mean = str(combined_mean/len(tup))
-    f_new_pos3.write("Mean: " + mean + "\n")
+    mean = combined_mean/len(tup)
+    distance = rssitodistance(mean, dr)
+    f_new_pos3.write("Mean: " + str(mean) + "\n")
+    f_new_pos3.write("Distance: " + str(distance) + "\n")
+    dr += 1
+
+##############################################################################################3
+
+#f_new_pos1.write("rec 1" + "\n")
+#dr = 0
+#for tup in extracted_data_rec1pos1:
+#    combined_mean = 0.0
+#    print("DR " + str(dr))
+#    f_new_pos1.write("DR " + str(dr) + "\n")
+#    dr += 1
+#    for tuptup in tup:
+#        print(tuptup)
+#        combined_mean += int(tuptup)
+
+#    print("Mean: " + str(combined_mean/len(tup)))
+#    mean = str(combined_mean/len(tup))
+#    f_new_pos1.write("Mean: " + mean + "\n")
+
+#f_new_pos1.write("rec 2" + "\n")
+#dr = 0
+#for tup in extracted_data_rec2pos1:
+#    combined_mean = 0.0
+#    print("DR " + str(dr))
+#    f_new_pos1.write("DR " + str(dr) + "\n")
+#    dr += 1
+#    for tuptup in tup:
+#        print(tuptup)
+#        combined_mean += int(tuptup)
+
+#    print("Mean: " + str(combined_mean/len(tup)))
+#    mean = str(combined_mean/len(tup))
+#    f_new_pos1.write("Mean: " + mean + "\n")
+
+#f_new_pos1.write("rec 3" + "\n")
+#dr = 0
+#for tup in extracted_data_rec3pos1:
+#    combined_mean = 0.0
+#    print("DR " + str(dr))
+#    f_new_pos1.write("DR " + str(dr) + "\n")
+#    dr += 1
+#    for tuptup in tup:
+#        print(tuptup)
+#        combined_mean += int(tuptup)
+
+#    print("Mean: " + str(combined_mean/len(tup)))
+#    mean = str(combined_mean/len(tup))
+#    f_new_pos1.write("Mean: " + mean + "\n")
+
+#f_new_pos2.write("rec 1" + "\n")
+#dr = 0
+#for tup in extracted_data_rec1pos2:
+#    combined_mean = 0.0
+#    print("DR " + str(dr))
+#    f_new_pos2.write("DR " + str(dr) + "\n")
+#    dr += 1
+#    for tuptup in tup:
+#        print(tuptup)
+#        combined_mean += int(tuptup)
+
+#    print("Mean: " + str(combined_mean/len(tup)))
+#    mean = str(combined_mean/len(tup))
+#    f_new_pos2.write("Mean: " + mean + "\n")
+
+#f_new_pos2.write("rec 2" + "\n")
+#dr = 0
+#for tup in extracted_data_rec2pos2:
+#    combined_mean = 0.0
+#    print("DR " + str(dr))
+#    f_new_pos2.write("DR " + str(dr) + "\n")
+#    dr += 1
+#    for tuptup in tup:
+#        print(tuptup)
+#        combined_mean += int(tuptup)
+
+#    print("Mean: " + str(combined_mean/len(tup)))
+#    mean = str(combined_mean/len(tup))
+#    f_new_pos2.write("Mean: " + mean + "\n")
+
+#f_new_pos2.write("rec 3" + "\n")
+#dr = 0
+#for tup in extracted_data_rec3pos2:
+#    combined_mean = 0.0
+#    print("DR " + str(dr))
+#    f_new_pos2.write("DR " + str(dr) + "\n")
+#    dr += 1
+#    for tuptup in tup:
+#        print(tuptup)
+#        combined_mean += int(tuptup)
+
+#    print("Mean: " + str(combined_mean/len(tup)))
+#    mean = str(combined_mean/len(tup))
+#    f_new_pos2.write("Mean: " + mean + "\n")
+
+
+#f_new_pos3.write("rec 1" + "\n")
+#dr = 0
+#for tup in extracted_data_rec1pos3:
+#    combined_mean = 0.0
+#    print("DR " + str(dr))
+#    f_new_pos3.write("DR " + str(dr) + "\n")
+#    dr += 1
+#    for tuptup in tup:
+#        print(tuptup)
+#        combined_mean += int(tuptup)
+
+#    print("Mean: " + str(combined_mean/len(tup)))
+#    mean = str(combined_mean/len(tup))
+#    f_new_pos3.write("Mean: " + mean + "\n")
+
+#f_new_pos3.write("rec 2" + "\n")
+#dr = 0
+#for tup in extracted_data_rec2pos3:
+#    combined_mean = 0.0
+#    print("DR " + str(dr))
+#    f_new_pos3.write("DR " + str(dr) + "\n")
+#    dr += 1
+#    for tuptup in tup:
+#        print(tuptup)
+#        combined_mean += int(tuptup)
+
+#    print("Mean: " + str(combined_mean/len(tup)))
+#    mean = str(combined_mean/len(tup))
+#    f_new_pos3.write("Mean: " + mean + "\n")
+
+#f_new_pos3.write("rec 3" + "\n")
+#dr = 0
+#for tup in extracted_data_rec3pos3:
+#    combined_mean = 0.0
+#    print("DR " + str(dr))
+#    f_new_pos3.write("DR " + str(dr) + "\n")
+#    dr += 1
+#    for tuptup in tup:
+#        print(tuptup)
+#        combined_mean += int(tuptup)
+
+#    print("Mean: " + str(combined_mean/len(tup)))
+#    mean = str(combined_mean/len(tup))
+#    f_new_pos3.write("Mean: " + mean + "\n")
 
 
 #################################################################################3
