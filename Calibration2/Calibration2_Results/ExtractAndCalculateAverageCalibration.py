d23f0065091c1ab8afbe35912291602646362945
AntonAhlinder <55693307+AntonAhlinder@users.noreply.github.com>
2022-05-03 09:50:25 +0200

    Calibration average (cont)

--- Calibration2/Calibration2_Results/ExtractAndCalculateAverageCalibration.py
+++ Calibration2/Calibration2_Results/ExtractAndCalculateAverageCalibration.py
@@ -1,24 +1,24 @@
 import re
 
-regex = "( [0-9]+).*(-[0-9]+).+\s.*(-[0-9]+).+\s.*(-[0-9]+).+\s.*(-[0-9]+).+\s.*(-[0-9]+).+\s.*(-[0-9]+).+\s"
-# regex_DR1_DR10 = "( [0-9]+).*(-[0-9]+).+\s.*.*(-[0-9]+).+\s.*.*(-[0-9]+).+\s.*.*(-[0-9]+).+\s.*.*(-[0-9]+).+\s"
+regex_6 = "( [0-9]+).*(-[0-9]+).+\s.*(-[0-9]+).+\s.*(-[0-9]+).+\s.*(-[0-9]+).+\s.*(-[0-9]+).+\s.*(-[0-9]+).+\s"
+regex_5 = "( [0-9]+).*(-[0-9]+).+\s.*.*(-[0-9]+).+\s.*.*(-[0-9]+).+\s.*.*(-[0-9]+).+\s.*.*(-[0-9]+).+\s"
 
 for DRx in range(11,32):
     f = open("C:\\Users\\myran\\Desktop\\Enchipsdatorer\\LoRa-Exjobb\\Calibration2\\Calibration2_Results\\Calibration_Raw_Data\\Calibration_DR" + str(DRx) + ".txt", encoding="utf-8")
     txt = f.read()
     f_new = open("C:\\Users\\myran\\Desktop\\Enchipsdatorer\\LoRa-Exjobb\\Calibration2\\Calibration2_Results\\Calibration_Average_RSSI\\Calibration_DR" + str(DRx) +"_average.txt", "w")
-    # if DRx == 0:
-    extracted_data = re.findall(regex, txt)
-    for distance in extracted_data:
-        dist_in_m = round(int(distance[0]) * 0.01, 1)
-        average_rssi = (int(distance[2]) + int(distance[3]) + int(distance[4]) + int(distance[5]) + int(distance[6])) / 5
-        f_new.write(str(dist_in_m) + ":" + str(average_rssi) + "\n")
-    # else:
-    #     extracted_data = re.findall(regex_DR1_DR10, txt)
-    #     for distance in extracted_data:
-    #         dist_in_m = round(int(distance[0]) * 0.01, 1)
-    #         average_rssi = (int(distance[1]) + int(distance[2]) + int(distance[3]) + int(distance[4]) + int(distance[5])) / 5
-    #         f_new.write(str(dist_in_m) + ":" + str(average_rssi) + "\n")
+    if DRx < 22:
+        extracted_data = re.findall(regex_6, txt)
+        for distance in extracted_data:
+            dist_in_m = round(int(distance[0]) * 0.01, 1)
+            average_rssi = (int(distance[2]) + int(distance[3]) + int(distance[4]) + int(distance[5]) + int(distance[6])) / 5
+            f_new.write(str(dist_in_m) + ":" + str(average_rssi) + "\n")
+    else:
+        extracted_data = re.findall(regex_5, txt)
+        for distance in extracted_data:
+            dist_in_m = round(int(distance[0]) * 0.01, 1)
+            average_rssi = (int(distance[1]) + int(distance[2]) + int(distance[3]) + int(distance[4]) + int(distance[5])) / 5
+            f_new.write(str(dist_in_m) + ":" + str(average_rssi) + "\n")
 
 
 # for DRx in range(11,32):
